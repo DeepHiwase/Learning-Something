563938550a6edc37b9df144fe634c1d25c2d395f
Deep <diphiwase0987@gmail.com>
2025-08-30 18:57:46 +0530

    js loops

---
+++ js/tempo/Loops/js1_loops.js
@@ -0,0 +1,96 @@
+// Loops -> while  loop && for loop
+// console.log("Hi");
+// console.log("Hi");
+// console.log("Hi");
+// console.log("Hi");
+// O(1)
+
+// Mouse app for mobile as touchpad
+
+// -> repeatation of code -> issue - taking space
+
+// while
+
+// let count = 0;
+// while (false) {
+//   count++;
+//   console.log(count);
+// }
+
+// let i = 1;
+// while (i <= 10 || i < 11 || i != 11) {
+//   console.log(i);
+//   i++;
+// }
+
+// let num = 1;
+// while (num <= 25) {
+//   if (num %2 === 0) console.log(num);
+//   num++;
+// }
+// // O(25/ 2) -> O(n)
+
+// let num = 10;
+
+// while (num > 0) {
+//   console.log(num);
+//   num--;
+// }
+// // O(10) -> O(n)
+
+// For loop
+// for (let index = 1; index <= 10; index++) {
+//   console.log("hi");
+// }
+// // O(10) -> O(1)
+
+// for (let index = 1; index <= 10; index++) {
+//   console.log(index);
+// }
+// // O(10) -> O(1)
+
+// for (let index = 3; index < 50; index++) {
+//   if (index % 3 === 0) console.log(index);
+// }
+// // O(50 / 3) -> O(1)
+
+// for (let index = 3; index < 50; index = index + 3) {
+//   console.log(index);
+// }
+// // O(50) -> O(1)
+
+// let n = 1;
+// for (let index = n; index * index < 35 && index * index >= 25; index++) {
+//   console.log(index * index);
+// }
+
+// let n = 35;
+// for (let index = 20; index <= n; index++) {
+//   console.log(index * index);
+// }
+// // O(n-20) -> O(n) -
+
+// let num = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(num, "x", i, "=", num * i);
+// }
+
+// O(n)
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// O(100) -> O(1)
+
+
+// 
+const n1 = false &&  5;
+const n2 = n1 === false;
+const n3 = n2 && "67";
+
+console.log({ n1, n2, n3 });
+
